7693cbe2ba2d03637e21976fc583438ab57ac801
shaizad534 <43028055+shaizad534@users.noreply.github.com>
2021-01-07 16:50:25 -0600

    Create output.tf
    Initial commit

---
+++ output.tf
@@ -0,0 +1,15 @@
+# Outputs.tf
+output "instance_id" {
+  description = " Instance ID of the instance"
+  value       = aws_instance.poc-server.id
+}
+
+output "instance_IP" {
+  description = " Public IP of the instance"
+  value       = aws_instance.poc-server.public_ip
+}
+
+output "Efs_id" {
+  description = " Efs id of the Elastic filesystem"
+  value       = aws_efs_file_system.poc-efs.id
+}
