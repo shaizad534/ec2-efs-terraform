ce53d6ec023b49bfe3e8027f6f7e234d8686b1e2
shaizad534 <43028055+shaizad534@users.noreply.github.com>
2021-01-08 16:59:31 -0600

    Update output.tf
    changed variables

--- output.tf
+++ output.tf
@@ -1,15 +1,46 @@
-# Outputs.tf
-output "instance_id" {
-  description = " Instance ID of the instance"
-  value       = aws_instance.poc-server.id
+# Variables TF File
+variable "region" {
+  description = "AWS Region "
+  default     = "us-east-1"
 }
 
-output "instance_IP" {
-  description = " Public IP of the instance"
-  value       = aws_instance.poc-server.public_ip
+variable "ami_id" {
+  description = "AMI ID to be used for Instance "
+  default     = "ami-0be2609ba883822ec"
 }
 
-output "Efs_id" {
-  description = " Efs id of the Elastic filesystem"
-  value       = aws_efs_file_system.poc-efs.id
+variable "instancetype" {
+  description = "Instance Type to be used for Instance "
+  default     = "t2.micro"
+}
+
+variable "subnetid" {
+  description = "Subnet ID to be used for Instance "
+  default     = "subnet-d291e59f"
+}
+
+variable "vpcid" {
+  description = "Vpc to be used for Instance "
+  default     = "vpc-4cbd4d31"
+}
+
+
+variable "AppName" {
+  description = "Application Name"
+  default     = "poc-server"
+}
+
+variable "Env" {
+  description = "Application Name"
+  default     = "Poc"
+}
+
+variable "HostIp" {
+  description = " Host IP to be allowed SSH for"
+  default     = "98.206.221.201/32"
+}
+
+variable "PvtIp" {
+  description = " subnet IP to be allowed SSH for"
+  default     = "172.31.16.0/20"
 }
